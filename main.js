67b9a8408ee6abc9ca995d08349b7729f3636d1c
JoseDanker <jidanker@miuandes.cl>
2025-03-21 19:26:38 -0300

    lab2 finished

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 
-let show_news = document.getElementById('noticias');
+
+const setupEventListeners = () => {
+	document.querySelector('.btn-group').addEventListener('click', inicioJuego)
+}
+
+function inicioJuego(){
+    let show_news = document.getElementById('noticias');
+    show_news.innerHTML = "";
 
 fetch('https://newsapi.org/v2/top-headlines?category=technology&apiKey=3019cb39a7ec4cf19461364c161388a5').then((resp) => resp.json()).then(dato => {
     console.log(dato);
     let noticias = (dato.articles);
-    console.log(dato.articles);
-    noticias.map(function(number) {
+    const top10 = noticias.slice(0,10);
+    top10.map(function(number) {
         let div = document.createElement('noticias');
-        div.innerHTML= `<br>    
-                        <img style = "max-width:800px" src=${number.urlToImage}></br>
-                        <h1>${number.title}</h1>
-                        <h2>${number.description}</h2>`;
+        div.innerHTML= `<div class="card text-white bg-secondary mb-3">
+                            <div class="card-body">
+                                <br>    
+                                <a href="${number.url}"><img style = "max-width:800px" src=${number.urlToImage} ></a></br>
+                                <h1>${number.title}</h1>
+                                <h2>${number.description}</h2>
+                            </div>
+                        </div>`;
+                        
         show_news.appendChild(div);
 
     })
 });
+}
+document.addEventListener('DOMContentLoaded', setupEventListeners);
